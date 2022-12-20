bc2e6b610a474902f3b58ac7f3262d26784703a5
Jonny Vickers <jonathanbvickers@gmail.com>
2022-12-20 17:13:30 +0000

    nearly there with the tyeps

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -1,22 +1,20 @@
 /// <reference types="cypress"/>
-import { navigateTo } from '../support/page_objects/navigationPage';
-import { mockData } from '../../mock/mockData';
-import { setupWorker, rest, SetupWorkerApi } from 'msw';
-import { handlers } from '../../mock/handlers';
+// import { mockData } from '../../mock/mockData';
+import { cy, it } from 'local-cypress';
 
 describe('First tests', () => {
-  before('mock api call', () => {
-    cy.window().then((window) => {
-      console.log(window);
-      const { worker, rest } = window.msw;
+  // before('mock api call', () => {
+  //   cy.window().then((window) => {
+  //     console.log(window);
+  //     const { worker, rest } = window.msw;
 
-      worker.use(
-        rest.get('/products', (_req, res, ctx) => {
-          return res(ctx.json(mockData));
-        })
-      );
-    });
-  });
+  //     worker.use(
+  //       rest.get('/products', (_req, res, ctx) => {
+  //         return res(ctx.json(mockData));
+  //       })
+  //     );
+  //   });
+  // });
 
   it('goes to wishlist', () => {
     cy.visit('/');
